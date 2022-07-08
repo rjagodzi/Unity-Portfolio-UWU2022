075e6fc2469d2cc9035bd631551dc09ca347e11c
Ret <retro44@gmail.com>
2022-07-09 01:15:11 +0200

    Parallax take 1

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [SerializeField] private Transform m_BallTransform;
+    [SerializeField] private List<Transform> m_Layers;
+    private Transform m_Transform;
+
+    [SerializeField] private float m_TopLayerHeight;
+    [SerializeField] private float m_BottomLayerHeight;
+
+    public void Start()
+    {
+        m_Transform = GetComponent<Transform>();
+     /*   m_Layers = new List<Transform>();
+        for(int i=0;i<m_Transform.childCount;i++)
+        {
+            m_Layers.Add(m_Transform.GetChild(i).GetComponent<Transform>());
+        }
+
+        m_Layers.Sort(delegate (Transform x, Transform y) {
+            if (x.position.z > y.position.z) return -1;
+            else if (x.position.z < y.position.z) return 1;
+            else return 0;
+        });*/
+    }
+
+    public void Update()
+    {
+        if (m_BallTransform.position.y >= 1f)
+        {
+
+        }
+        else
+        {
+            for(int i=0;i<m_Layers.Count;i++)
+            {
+                Transform layer = m_Layers[i];
+                layer.position = new Vector3(layer.position.x, Mathf.Lerp(m_TopLayerHeight, m_BottomLayerHeight, i+1 * 1/m_Layers.Count), layer.position.z);
+            }
+        }
+    }
+}
